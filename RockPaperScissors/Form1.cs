c4788090741c90d69e07405e836becba61448c2b
opilane <opilane@HP-CZC6297BB7>
2021-12-08 13:59:57 +0200

    fixed the game, fixed 2 player, changed the design

--- RockPaperScissors/Form1.cs
+++ RockPaperScissors/Form1.cs
@@ -18,14 +18,16 @@ namespace RockPaperScissors
         Random rng = new Random();
         PictureBox pic;
         string[] choice = new string[2];
-        string[] rps = { "rock","paper","scissors"};
+        string[] rps = { "rock", "paper", "scissors"};
         string[] results = new string[11];
         bool bot = true;
         Label lbl;
+        ListBox list;
         public Form1()
         {
-            resultsRead();
-            this.Size = new Size(840,500);
+            //resultsRead();
+            choice[0] = "rock";
+            this.Size = new Size(500,600);
             this.Text = "Rock Papers Scissors";
             this.BackColor = Color.LightCyan;
             this.ForeColor = Color.Black;
@@ -34,7 +36,8 @@ namespace RockPaperScissors
 
             lbl = new Label();
             lbl.Size = new Size(100, 25);
-            lbl.Location = new Point(30, 30);
+            lbl.Location = new Point(180, 25);
+            lbl.TextAlign = (ContentAlignment)HorizontalAlignment.Center;
             this.Controls.Add(lbl);
             
             pic = new PictureBox();
@@ -68,40 +71,45 @@ namespace RockPaperScissors
             pic.Image = Properties.Resources.rock1;
             pic.SizeMode = PictureBoxSizeMode.StretchImage;
             pic.Size = new Size(100, 100);
-            pic.Location = new Point(560, 70);
+            pic.Location = new Point(350, 70);
             pic.Click += Pic_Click;
             pic.Name = "rock2";
             this.Controls.Add(pic);
+            pic.Hide();
 
             pic = new PictureBox();
             pic.Image = Properties.Resources.paper1;
             pic.SizeMode = PictureBoxSizeMode.StretchImage;
             pic.Size = new Size(100, 100);
-            pic.Location = new Point(560, 190);
+            pic.Location = new Point(350, 190);
             pic.Click += Pic_Click;
             pic.Name = "paper2";
             this.Controls.Add(pic);
+            pic.Hide();
 
             pic = new PictureBox();
             pic.Image = Properties.Resources.scissors1;
             pic.SizeMode = PictureBoxSizeMode.StretchImage;
             pic.Size = new Size(100, 100);
-            pic.Location = new Point(560, 300);
+            pic.Location = new Point(350, 300);
             pic.Click += Pic_Click;
             pic.Name = "scissors2";
             this.Controls.Add(pic);
-            Console.WriteLine(pic.Name.Substring(pic.Name.Length), "meeeooww");
+            pic.Hide();
 
             Button btn = new Button();
             btn.Size = new Size(100,50);
-            btn.Location = new Point(320, 25);
+            btn.Location = new Point(180, 50);
             btn.Click += Btn_Click;
             btn.Text = "Begin Battle";
             btn.FlatAppearance.BorderSize = 0;
             btn.BackColor = this.BackColor;
             this.Controls.Add(btn);
 
-
+            list = new ListBox();
+            list.Size = new Size(100,200);
+            list.Location = new Point(180, 110);
+            this.Controls.Add(list);
             MainMenu menu = new MainMenu();
             MenuItem mf = new MenuItem("Settings");
             mf.MenuItems.Add("2 Player Mode", new EventHandler(menuFile_Select3)).Shortcut = Shortcut.CtrlS;
@@ -109,7 +117,6 @@ namespace RockPaperScissors
             mf.MenuItems.Add("Exit", new EventHandler(menuFile_Select));
             menu.MenuItems.Add(mf);
             this.Menu = menu;
-            hiSh2player();
         }
 
         private void menuFile_Select3(object sender, EventArgs e)
@@ -123,7 +130,6 @@ namespace RockPaperScissors
             m.Checked = !m.Checked;
             if (m.Checked==true)
             {
-                this.BackColor = Color.DarkBlue;
                 this.ForeColor = Color.Gray;
             }
             else
@@ -184,10 +190,12 @@ namespace RockPaperScissors
 
         private void Btn_Click(object sender, EventArgs e)
         {
-            if (bot)
+            Console.WriteLine(rps[0]);
+            if (bot==true)
             {
-                choice[1] = rps[rng.Next(0,rps.Length)];
+                choice[1] = rps[rng.Next(0,3)];
             }
+            Console.WriteLine(choice[0],',',choice[1],',', rps[rng.Next(0, rps.Length)],',',bot);
             if (win()==0)
             {
                 lbl.Text = "Opponent win";
@@ -227,7 +235,7 @@ namespace RockPaperScissors
                     }
                 }
             }
-            resultsWrite();
+            //resultsWrite();
         }
         private void menuFile_Select(object sender, EventArgs e)
         {
@@ -277,13 +285,14 @@ namespace RockPaperScissors
         }
         private void hiSh2player()
         {
+            bot = !bot;
             foreach (Control c in this.Controls)
             {
                 Console.WriteLine(c);
                 if (c is PictureBox)
                 {
                     pic = c as PictureBox;
-                    if (pic.Name.Substring(pic.Name.Length - 1)=="2" && bot==false)
+                    if (pic.Name.Substring(pic.Name.Length - 1)== "2" && bot==false)
                     {
                         pic.Show();
                     }
@@ -293,27 +302,26 @@ namespace RockPaperScissors
                     }
                 }
             }
-            bot = !bot;
         }
-        private void resultsWrite()
-        {
-            using (StreamWriter file = new StreamWriter(@"../../Resources/results.txt"))
-            {
-                foreach (var item in results)
-                {
-                    file.WriteLine(item);
-                }
-            }
-        }
-        private void resultsRead()
-        {
-            using (StreamReader file = new StreamReader(@"../../Resources/results.txt"))
-            {
-                for (int i = 0; i < results.Length; i++)
-                {
-                    results[i]= file.ReadLine();
-                }
-            }
-        }
+        //private void resultsWrite()
+        //{
+        //    using (StreamWriter file = new StreamWriter(@"../../Resources/results.txt"))
+        //    {
+        //        foreach (var item in results)
+        //        {
+        //            file.WriteLine(item);
+        //        }
+        //    }
+        //}
+        //private void resultsRead()
+        //{
+        //    using (StreamReader file = new StreamReader(@"../../Resources/results.txt"))
+        //    {
+        //        for (int i = 0; i < results.Length; i++)
+        //        {
+        //            results[i]= file.ReadLine();
+        //        }
+        //    }
+        //}
     }
 }
